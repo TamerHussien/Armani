c8c5ec78e0695149061ef1cfccd15b54e4122c35
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2022-08-25 22:51:37 +0200

    make animation start auto add the right menu link add address and phone info

---
+++ client.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var toggle = document.getElementById('toggle');
+
+    setTimeout(() => {
+       toggle.checked = true 
+    }, 500);
+})
